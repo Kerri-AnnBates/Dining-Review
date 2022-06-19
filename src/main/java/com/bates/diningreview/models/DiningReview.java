7629437ee6dbfb79da9267d835575ac31d5c87ad
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-18 21:31:51 -0400

    add review entity

---
+++ src/main/java/com/bates/diningreview/models/DiningReview.java
@@ -0,0 +1,26 @@
+package com.bates.diningreview.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Review")
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class DiningReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String username;
+    private Long restaurantId;
+    private Integer peanutScore;
+    private Integer eggScore;
+    private Integer dairyScore;
+    private String comment;
+}
