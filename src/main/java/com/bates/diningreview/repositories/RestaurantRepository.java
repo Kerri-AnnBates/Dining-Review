87e472422d184f446a36e0e999332b5182ae536a
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-22 09:57:57 -0400

    remove broken query methods

--- src/main/java/com/bates/diningreview/repositories/RestaurantRepository.java
+++ src/main/java/com/bates/diningreview/repositories/RestaurantRepository.java
@@ -3,10 +3,5 @@ package com.bates.diningreview.repositories;
 import com.bates.diningreview.models.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
-    List<Restaurant> findByZipcodeAndPeanutScoreGreaterThanEqual(String zip, Integer score);
-    List<Restaurant> findByZipcodeAndEggScoreGreaterThanEqual(String zip, Integer score);
-    List<Restaurant> findByZipcodeAndDairyScoreGreaterThanEqual(String zip, Integer score);
 }
