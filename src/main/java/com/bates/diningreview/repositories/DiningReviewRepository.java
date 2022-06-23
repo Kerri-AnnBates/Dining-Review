d05555ff369a142ac9ee99084be108a22624fa1c
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-23 13:32:14 -0400

    update review repo

--- src/main/java/com/bates/diningreview/repositories/DiningReviewRepository.java
+++ src/main/java/com/bates/diningreview/repositories/DiningReviewRepository.java
@@ -3,5 +3,9 @@ package com.bates.diningreview.repositories;
 import com.bates.diningreview.models.DiningReview;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
+    List<DiningReview> findByStatusIs(String status);
+    List<DiningReview> findByRestaurantIdAndStatusIs(Long id, String status);
 }
