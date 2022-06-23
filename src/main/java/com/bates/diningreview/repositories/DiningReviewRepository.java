0b661a67c66f53aa346bda7dd3630d2b6e548217
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-23 13:34:45 -0400

    fix query params

--- src/main/java/com/bates/diningreview/repositories/DiningReviewRepository.java
+++ src/main/java/com/bates/diningreview/repositories/DiningReviewRepository.java
@@ -1,11 +1,12 @@
 package com.bates.diningreview.repositories;
 
 import com.bates.diningreview.models.DiningReview;
+import com.bates.diningreview.models.Status;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
-    List<DiningReview> findByStatusIs(String status);
-    List<DiningReview> findByRestaurantIdAndStatusIs(Long id, String status);
+    List<DiningReview> findByStatusIs(Status status);
+    List<DiningReview> findByRestaurantIdAndStatusIs(Long id, Status status);
 }
