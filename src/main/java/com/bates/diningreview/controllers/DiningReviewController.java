0ad04aee2e1e99c799518a9b87faa8110763362c
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-23 12:17:56 -0400

    submit a review

--- src/main/java/com/bates/diningreview/controllers/DiningReviewController.java
+++ src/main/java/com/bates/diningreview/controllers/DiningReviewController.java
@@ -1,4 +1,24 @@
 package com.bates.diningreview.controllers;
 
+import com.bates.diningreview.models.DiningReview;
+import com.bates.diningreview.repositories.DiningReviewRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
 public class DiningReviewController {
+    private final DiningReviewRepository diningReviewRepository;
+
+    public DiningReviewController(DiningReviewRepository diningReviewRepository) {
+        this.diningReviewRepository = diningReviewRepository;
+    }
+
+    // Allow user to submit a review
+    @PostMapping("")
+    public DiningReview addReview(@RequestBody DiningReview newReview) {
+        return diningReviewRepository.save(newReview);
+    }
 }
