056fa3b5e94ff6818048bdbc51167eb6f9f658b5
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-06-25 17:18:53 -0400

    update review controller with validations

--- src/main/java/com/bates/diningreview/controllers/DiningReviewController.java
+++ src/main/java/com/bates/diningreview/controllers/DiningReviewController.java
@@ -2,8 +2,10 @@ package com.bates.diningreview.controllers;
 
 import com.bates.diningreview.models.AdminReview;
 import com.bates.diningreview.models.DiningReview;
+import com.bates.diningreview.models.Restaurant;
 import com.bates.diningreview.models.Status;
 import com.bates.diningreview.repositories.DiningReviewRepository;
+import com.bates.diningreview.repositories.RestaurantRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,19 +17,27 @@ import java.util.Optional;
 @RequestMapping("/api/reviews")
 public class DiningReviewController {
     private final DiningReviewRepository diningReviewRepository;
+    private final RestaurantRepository restaurantRepository;
 
-    public DiningReviewController(DiningReviewRepository diningReviewRepository) {
+    public DiningReviewController(DiningReviewRepository diningReviewRepository, RestaurantRepository restaurantRepository) {
         this.diningReviewRepository = diningReviewRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
     // Allow user to submit a review
     @PostMapping("")
     public DiningReview addReview(@RequestBody DiningReview newReview) {
+        Optional<Restaurant> restOpt = restaurantRepository.findById(newReview.getRestaurantId());
+
+        if (restOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This restaurant does not exists");
+        }
+
         return diningReviewRepository.save(newReview);
     }
 
     // As an admin, I want to approve or reject a given dining review.
-    @PutMapping("admin/{reviewId}")
+    @PutMapping("admin/review/{reviewId}")
     public DiningReview updateReview(@PathVariable Long reviewId, @RequestBody AdminReview adminReview) {
         Optional<DiningReview> reviewOpt = diningReviewRepository.findById(reviewId);
 
@@ -47,7 +57,7 @@ public class DiningReviewController {
     }
 
     // Get pending reviews
-    @GetMapping("admin/pending")
+    @GetMapping("admin/review/pending")
     public List<DiningReview> getPendingReviews() {
         return diningReviewRepository.findByStatusIs(Status.PENDING);
     }
